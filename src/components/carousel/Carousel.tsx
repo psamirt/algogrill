4800677abc7a1beff43b576ba7e9b4723a47c4b8
psamirt <p_samir@hotmail.com>
2023-08-29 19:23:44 -0500

    carousel principal

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,24 +1,57 @@
-import './Carousel.css'
+import React, { useState } from 'react'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import { RxDotFilled } from 'react-icons/rx'
 
 const Carousel: React.FC = () => {
+  const slides = [
+    { url: '/imagenes/hamburguesa.png' },
+    { url: '/imagenes/alitas.png' },
+    { url: '/imagenes/pack2.png' },
+    { url: '/imagenes/salchigrill.png' },
+    { url: '/imagenes/presentacion.png' }
+  ]
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const prevSlide = (): void => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
+    setCurrentIndex(newIndex)
+  }
+  const nextSlide = (): void => {
+    const isLastSlide = currentIndex === slides.length - 1
+    const newIndex = isLastSlide ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex)
+  }
+
+  const goToSlide = (slideIndex: number): void => {
+    setCurrentIndex(slideIndex)
+  }
+
   return (
-    <div className="container-carousel">
-      <div className="carousel">
-        <article className="card">
-    <img src="/imagenes/hamburguesa.png" alt="" />
-        </article>
-        <article className="card">
-        <img src="/imagenes/alitas.png" alt="" />
-        </article>
-        <article className="card">
-        <img src="/imagenes/salchigrill.png" alt="" />
-        </article>
-        <article className="card">
-        <img src="/imagenes/pack2.png" alt="" />
-        </article>
-        <article className="card">
-        <img src="/imagenes/presentacion.png" alt="" />
-        </article>
+    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          className={`w-full h-full rounded-2x1 bg-center bg-cover duration-500 ${index === currentIndex ? '' : 'hidden'}`}
+        >
+          <img src={slide.url} alt={`Slide ${index}`} className='w-full h-full' />
+        </div>
+      ))}
+      {/* flecha izquierda */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* flecha derecha */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slide, slideIndex) => (
+          <div className='text-2xl cursor-pointer' key={slideIndex} onClick={(): void => { goToSlide(slideIndex) }}>
+            <RxDotFilled />
+          </div>
+        ))}
       </div>
     </div>
   )
