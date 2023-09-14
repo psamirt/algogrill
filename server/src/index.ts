5b0ee3f2e04fe2b139ad128e1bacc9998334e1ea
psamirt <p_samir@hotmail.com>
2023-09-13 19:18:59 -0500

    instalacion de backend

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app= express()
+app.use(express.json())
+
+const PORT = 3000
+
+app.get('/product', (_req, res)=>{
+    console.log('Esta corriendo el server')
+    res.send('probanding')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
