f88226bb7bc53670198c35f790131b330e9f7136
psamirt <p_samir@hotmail.com>
2023-09-15 19:05:10 -0500

    Remove ignored files

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('./database/db');
+const express = require('express');
+const app = express();
+const cors = require('cors')
+require('dotenv').config()
+const routes = require('./routes/productsRouter')
+
+const {PORT} = process.env
+
+app.use(cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); //Autorizo recibir solicitudes de este dominio
+  res.header("Access-Control-Allow-Credentials", true); //Autorizo recibir solicitudes que incluyan el encabezado con credenciales
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); //Autorizo recibir solicitudes con dichos headers
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE"); //Autorizo las solicitudes tipo GET, POST, OPTIONS, PUT y DELETE.
+  next(); // para pasar el control al siguiente middleware
+});
+
+app.use('/', routes);
+
+app.get('/', (req,res)=>{
+  res.status(200).send('corriendo')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
