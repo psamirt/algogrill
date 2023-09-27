d350a7ee6b63a79b673c2ff64138074be7cb73b5
psamirt <p_samir@hotmail.com>
2023-09-26 22:56:58 -0500

    auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,23 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Burger from './pages/Burger'
-import ScrollToTop from "react-scroll-to-top";
-import Login from './components/modal/Login';
+import ScrollToTop from 'react-scroll-to-top'
+import Login from './components/modal/Login'
+import { AuthProvider } from './context/authContext'
 
 const App = (): JSX.Element => {
 	return (
-		<div>
-			<Navbar />
-			<Routes>
-				<Route path='/' element={<Home />}></Route>
-				<Route path='/menu' element={<Burger />}></Route>
-				<Route path='/login' element={<Login />}></Route>
-			</Routes>
-			<ScrollToTop smooth />
-		</div>
+		<AuthProvider>
+			<div>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+					<Route path='/menu' element={<Burger />}></Route>
+					<Route path='/login' element={<Login />}></Route>
+				</Routes>
+				<ScrollToTop smooth />
+			</div>
+		</AuthProvider>
 	)
 }
 
---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useContext, ReactNode } from 'react'
+import { auth } from '../utils/firebaseConfig'
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+	UserCredential
+} from 'firebase/auth'
+
+interface IAuthContext {
+	register: (email: string, password: string) => Promise<void>,
+	login: (email: string, password: string) => Promise<void>,
+	loginWithGoogle: () => Promise<UserCredential>,
+	logout: () => Promise<void>
+}
+
+export const authContext = createContext<IAuthContext | undefined>(undefined)
+
+export const useAuth = (): IAuthContext => {
+	const context = useContext(authContext)
+	if (!context) {
+		throw new Error('useAuth debe estar dentro del proveedor AuthContext')
+	}
+	return context
+}
+
+interface IAuthProviderProps {
+	children: ReactNode
+}
+
+export function AuthProvider({ children }: IAuthProviderProps) {
+	const register = async (email: string, password: string) => {
+		const response = await createUserWithEmailAndPassword(auth, email, password)
+		console.log(response)
+	}
+	const login = async (email: string, password: string) => {
+		const response = await signInWithEmailAndPassword(auth, email, password)
+		console.log(response)
+	}
+	const loginWithGoogle = async () => {
+		const responseGoogle = new GoogleAuthProvider()
+		return signInWithPopup(auth, responseGoogle)
+	}
+	const logout = async () => {
+		const responseOut = await signOut(auth)
+		console.log(responseOut)
+	}
+	return (
+		<authContext.Provider value={{ register, login, loginWithGoogle, logout }}>
+			{children}
+		</authContext.Provider>
+	)
+}
