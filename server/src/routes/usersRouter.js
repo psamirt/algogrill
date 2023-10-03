dbb21b730b289bf1c4a81d7450b06c4ec3b450ba
psamirt <p_samir@hotmail.com>
2023-10-02 21:46:44 -0500

    backend

--- server/src/routes/usersRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createUser, updateUser } = require('../controllers/usersController');
-
-//------------ nuevo usuario --------------
-router.post('/newUser', createUser);
-router.put('/updateUser/:id', updateUser)
-
-module.exports = router;
