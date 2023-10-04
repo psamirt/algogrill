f9e7e3cf1aee685cef6584ad198ce041682bc68c
psamirt <116773966+psamirt@users.noreply.github.com>
2023-10-04 18:43:00 -0500

    asd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,15 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import { Route, Routes } from 'react-router-dom'
 import Burger from './pages/Burger'
-import ScrollToTop from 'react-scroll-to-top'
 import { AuthProvider } from './context/AuthContext'
 import Register from './pages/user/Register'
 import ResetPassword from './pages/user/ResetPassword'
-import Dashboard from './pages/dashboard/dashboard'
+import Dashboard from './pages/dashboard/Dashboard'
 
 const App = (): JSX.Element => {
 	return (
 		<AuthProvider>
-		<Navbar />
+			<Navbar />
 			<Routes>
 				<Route path='/' element={<Home />}></Route>
 				<Route path='admin' element={<Dashboard />}></Route>
@@ -19,7 +18,6 @@ const App = (): JSX.Element => {
 				<Route path='/register' element={<Register />}></Route>
 				<Route path='/resetPassword' element={<ResetPassword />}></Route>
 			</Routes>
-			<ScrollToTop smooth />
 		</AuthProvider>
 	)
 }
