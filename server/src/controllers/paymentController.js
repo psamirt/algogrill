c0d51813e56bfd9b9735c3e49dbc48556402f8bb
psamirt <116773966+psamirt@users.noreply.github.com>
2023-11-14 19:52:59 -0500

    fg

--- server/src/controllers/paymentController.js
+++ server/src/controllers/paymentController.js
@@ -55,10 +55,6 @@ export const createOrder = async (req, res) => {
         notification_url: `https://algo-grill.onrender.com/order/webHook`,
         total_amount: parseFloat(totalAmount.toFixed(2)),
         auto_return: 'approved',
-        payer: {
-          phone: phoneNumber,
-          address: address
-        }
       };
 
       const result = await mercadopago.preferences.create(preference);
