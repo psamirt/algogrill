faaa57d15b26184f013e3beb85ddf494a0d44d4b
psamirt <p_samir@hotmail.com>
2023-11-17 17:00:48 -0500

    motion en home

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,11 +1,21 @@
 import CardHome from '../../components/card/CardHome'
 import Carousel from '../../components/carousel/Carousel'
+import { motion } from 'framer-motion'
+
 const Home = (): JSX.Element => {
 	return (
-		<div className='max-w-[1400px] bg-slate-50 m-auto h-[88vh]'>
+		<motion.div
+			className='max-w-[1400px] bg-slate-50 m-auto h-[88vh]'
+			initial={{
+				opacity: 0,
+				translateX: 90,
+			}}
+			animate={{ opacity: 1, translateX: 0, translateY: 0 }}
+			transition={{ duration: 1.2, delay: 0.3 }}
+		>
 			<Carousel></Carousel>
 			<CardHome />
-		</div>
+		</motion.div>
 	)
 }
 
