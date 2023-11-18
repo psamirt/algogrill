520958b2114929cd1a3994e428269b2d65d79b83
psamirt <p_samir@hotmail.com>
2023-11-18 16:47:33 -0500

    dashboard pagos totales

---
+++ client/src/app/redux/actions/adminAction.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+const baseUrl = import.meta.env.VITE_BASE_URL
+
+export const getPayed = async () => {
+	try {
+		const response = await axios.get(`${baseUrl}/order/getPayed`)
+	   return response.data
+	} catch (error) {
+		console.error('Error al obtener los pagos', error)
+	}
+}
--- client/src/pages/admin/dashboard/Dashboard.tsx
+++ client/src/pages/admin/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
 	BadgeDelta,
 	Card,
@@ -8,24 +8,39 @@ import {
 	Text,
 } from '@tremor/react'
 import { motion } from 'framer-motion'
+import { getPayed } from '../../../app/redux/actions/adminAction'
 
 const Dashboard: React.FC = () => {
+	const [payedData, setPayedData] = useState<number | undefined>(undefined)
+
+	useEffect(() => {
+		const getTotalPayed = async () => {
+			try {
+				const data = await getPayed()
+				setPayedData(data.totalPaid)
+			} catch (error) {
+				console.error('Error al obtener los pagos:', error)
+			}
+		}
+		getTotalPayed()
+	}, [])
+
 	return (
 		<motion.div
-        className='max-w-[1400px] mx-auto flex justify-between items-center p-4'
-				initial={{ opacity: 0, scale: 0.5 }}
-				animate={{ opacity: 1, scale: 1 }}
-				transition={{
-					duration: 0.8,
-					delay: 0.5,
-					ease: [0, 0.71, 0.2, 1.01],
-				}}
-        >
+			className='max-w-[1400px] mx-auto flex justify-between items-center p-4'
+			initial={{ opacity: 0, scale: 0.5 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{
+				duration: 0.8,
+				delay: 0.5,
+				ease: [0, 0.71, 0.2, 1.01],
+			}}
+		>
 			<Card className='max-w-lg mx-auto'>
 				<Flex alignItems='start'>
 					<div>
 						<Text>Ventas</Text>
-						<Metric>./S 12,699</Metric>
+						<Metric>./S {payedData}</Metric>
 					</div>
 					<BadgeDelta deltaType='moderateIncrease'>13.2%</BadgeDelta>
 				</Flex>
